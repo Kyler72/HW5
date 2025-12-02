f380ff0cc21653b46390f1509f75da5bc0b474cc
Kyle M. Ruszkowski <kyle.ruszk@gmail.com>
2025-12-01 20:32:37 -0700

    mutating data set to group by solved and unsolved

--- HW5.R
+++ HW5.R
@@ -9,6 +9,13 @@ options(tigris_use_cache = TRUE, tigris_class = "sf")
 
 homicide_df <- read_csv("data/homicide-data.csv")
 
-denver_homiicide_df <- homicide_df %>% 
+denver_homicide_df <- homicide_df %>% 
   filter(city == "Denver",
-         state == "CO")
+         state == "CO") %>%
+  mutate(
+    solved_status = case_when(
+      disposition == "Closed by arrest" ~ "Solved",
+      disposition %in% c("Closed without arrest",
+                         "Open/No arrest") ~ "Unsolved",
+      TRUE ~ NA_character_),
+    race_group = fct_lump(victim_race, n = 3))
